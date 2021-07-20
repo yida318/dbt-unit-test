65c2ee91a78dc004ccd713879c68bff5251a3382
易达 <da.yi@rootclould.com>
2021-07-20 14:28:15 +0800

    fix bug for comparing models

--- dbt_unit_test/templates/diff_macro.sql
+++ dbt_unit_test/templates/diff_macro.sql
@@ -1,3 +1,31 @@
 {% macro test_diff(model, test) %}
-select {{ compare_table(model, test) }}
+{% set columns = adapter.get_columns_in_relation(model) %}
+
+{% do run_query('SET join_use_nulls = 1') %}
+
+with extra_rows as(
+	select 1
+	from {{ model }} as table1
+	left outer join {{ test }} as table2 on
+	{% for i in columns %}
+	table1.{{ adapter.quote(i.column)|trim }} = table2.{{ adapter.quote(i.column)|trim }}
+	{% if not loop.last %}and{% endif %}
+	{% endfor %}
+	where table2.{{ adapter.quote(columns[0].column)|trim }} is null
+),
+missing_rows as(
+	select 1
+	from {{ test }} as table1
+	left outer join {{ model }} as table2 on
+	{% for i in columns %}
+	table1.{{ adapter.quote(i.column)|trim }} = table2.{{ adapter.quote(i.column)|trim }}
+	{% if not loop.last %}and{% endif %}
+	{% endfor %}
+	where table2.{{ adapter.quote(columns[0].column)|trim }} is null
+)
+
+select count(*)
+from(
+	select * from extra_rows union all select * from missing_rows
+)
 {% endmacro %}
--- dbt_unit_test/templates/example_test/model.sql
+++ dbt_unit_test/templates/example_test/model.sql
@@ -1,16 +1,10 @@
-{{
-    config(
-        materialized='view'
-    )
-}}
-with base as (
-    
-    select 
-        grp, 
-        sum(metric) as metric,
-        max(batch) as batch
-    from {{ ref('example_test_input') }} a
-    group by grp
+{{ config(materialized='view') }}
+
+with base as(
+	select	sum(a) as a,
+		b
+	from {{ ref('example_test_input') }}
+	group by b
 )
 
 select * from base
